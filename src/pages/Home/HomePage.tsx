0983f8fdb8819d000455a3eae44efd20b76738cd
Gavrilescu_Liviu <119134348+LiviuGav@users.noreply.github.com>
2025-12-07 19:10:28 +0200

    Enable Authentication

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,16 +1,15 @@
 import { StudentDashboard } from "../Student/StudentDashboard";
-//import { useAuthStore } from "@/stores/authStore";
-//import { Navigate } from "react-router-dom";
+import { useAuthStore } from "@/stores/authStore";
+import { Navigate } from "react-router-dom";
 
-export default function HomePage() {
-  /*
+export default function HomePage() {  
   const { isAuthenticated } = useAuthStore();
 
   // Dacă nu e logat → trimite la login
   if (!isAuthenticated) {
     return <Navigate to="/auth/login" replace />;
   }
-  */
+
   // Dacă e logat → arată pagina ta de student
   return <StudentDashboard />;
 }
