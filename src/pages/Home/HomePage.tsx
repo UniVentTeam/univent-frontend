7e52ebf4a3fb4f139b5734395eaab1710b3c3405
Ioan Motrescu <motrescuioan@gmail.com>
2025-12-15 17:09:51 +0200

    fix auto redirect on main page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,14 +1,14 @@
-import { StudentDashboard } from "../Student/StudentDashboard";
-import { useAuthStore } from "@/stores/authStore";
-import { Navigate } from "react-router-dom";
+import { StudentDashboard } from '../Student/StudentDashboard';
+// import { useAuthStore } from '@/stores/authStore';
+// import { Navigate } from 'react-router-dom';
 
-export default function HomePage() {  
-  const { isAuthenticated } = useAuthStore();
+export default function HomePage() {
+  // const { isAuthenticated } = useAuthStore();
 
   // Dacă nu e logat → trimite la login
-  if (!isAuthenticated) {
-    return <Navigate to="/auth/login" replace />;
-  }
+  // if (!isAuthenticated) {
+  //   return <Navigate to="/auth/login" replace />;
+  // }
 
   // Dacă e logat → arată pagina ta de student
   return <StudentDashboard />;
